9c539069257b8b2efa6fef678e91b0b8c5543531
ikar-zindo <plag.ikar@gmail.com>
2023-05-17 13:57:28 +0200

    add lifes

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
-
 const count_score = document.querySelector("#count_score");  // Ищем объект Луна
+const count_life = document.querySelector("#count_life");  // Ищем кол-во жизней
 const area = document.querySelector('#area'); // Ищем объект игровое поле
 const gameOver = document.querySelector(".game-over") // Ищем сообшение Game Over
 
 let score = 0; // очки
+let life = 3; // жизни
 let object = document.querySelector('#object'); // Ищем объект Луна
 const hit_sound = new Audio('sounds/hit.wav'); // Звук
 const miss_sound = new Audio('sounds/miss.wav'); // Звук
@@ -21,7 +22,9 @@ function start_game() {
    object.classList.toggle('start');
    gameOver.style.display = "none";
    score = 0; // обнуление очков
+   life = 3; // обнуление жизней
    count_score.innerText = `0`;
+   count_life.innerText = `3`;
    game_level_music.currentTime = 0;
    game_level_music.play();  // Звук
 }
@@ -34,6 +37,11 @@ function hit() { // дойствие при попадании
    void object.offsetWidth; // перекомпоновка DOM (API)
    object.classList.add('start');
 
+   if(score % 5 == 0) { // зарабатываем жизнь при наборе 5 очков
+      life++;
+      count_life.innerText = `${life}`;
+   }
+
    // Случайное появление Луны
    let random_offset = Math.floor(Math.random() * 400);
    object.style.left = `${random_offset}px`;
@@ -44,9 +52,9 @@ function hit() { // дойствие при попадании
 
 function miss(event) { // дойствие при промахивание
    if (event.target.id == "area" || event.target.id == "wrapper-game-over" ) {
-      score--; 
-      count_score.innerText = `${score}`;
-         if (score < 0) {
+      life--; 
+      count_life.innerText = `${life}`;
+         if (life < 0) {
             finish_game();
          }
       miss_sound.currentTime = 0; // Звук
@@ -55,9 +63,9 @@ function miss(event) { // дойствие при промахивание
 }
 
 function finish_game() {
-   const gameOver = document.querySelector(".game-over");
    object.classList.remove('start');
    count_score.innerText = `0`;
+   count_life.innerText = `3`;
    gameOver.style.display = "block";
 
    game_level_music.pause(); // Звук
